06eb4624551bbaff47cd783ce14542818817e60e
Cubby DeBry <cubbydebry@gmail.com>
2025-04-11 14:58:22 -0600

    Removed UART2 functions.

--- Src/lab1.c
+++ Src/lab1.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 UART_HandleTypeDef huart1;
-UART_HandleTypeDef huart2;
 
 #define DATA_PORT GPIOC
 #define ADDR_LOW_PORT GPIOC   // PC0-PC15
@@ -13,9 +12,7 @@ UART_HandleTypeDef huart2;
 
 void GPIO_Init_All(void);
 void GPIO_UART1_Init(void);
-void GPIO_UART2_Init(void);
 void UART1_Init(void);
-void UART2_Init(void);  // Initialize UART2 for USB debug
 void set_address(uint16_t addr);
 uint8_t read_data_byte(void);
 void send_serial(const char* msg);
@@ -24,9 +21,7 @@ int lab1_main(void) {
     HAL_Init();
     GPIO_Init_All();
     UART1_Init();
-    UART2_Init();
     GPIO_UART1_Init();
-    GPIO_UART2_Init();
 
     char msg[64];
 
@@ -104,19 +99,6 @@ void GPIO_UART1_Init(void) {
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
-void GPIO_UART2_Init(void) {
-    __HAL_RCC_GPIOA_CLK_ENABLE(); // PA2, PA3
-
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF1_USART2;
-
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
-
 void UART1_Init(void) {
     __HAL_RCC_USART1_CLK_ENABLE();
 
@@ -130,20 +112,6 @@ void UART1_Init(void) {
     HAL_UART_Init(&huart1);
 }
 
-void UART2_Init(void) {
-    __HAL_RCC_USART2_CLK_ENABLE();
-
-    huart2.Instance = USART2;
-    huart2.Init.BaudRate = 115200;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
-    huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
-    huart2.Init.Mode = UART_MODE_TX_RX;
-    huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-    huart2.Init.OverSampling = USART_OVERSAMPLING_16;
-    HAL_UART_Init(&huart2);
-}
-
 void send_serial(const char* msg) {
     HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
 }
@@ -151,7 +119,7 @@ void send_serial(const char* msg) {
 // Allow printf over USART
 
 int __io_putchar(int ch) {
-    HAL_UART_Transmit(&huart2, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
     return ch;
 }
 
