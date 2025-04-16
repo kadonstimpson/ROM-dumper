057d85aacf790b4a29b0c58c36d4b17cb9bc905b
Kadon Stimpson <kadonstimpson@gmail.com>
2025-04-16 11:24:12 -0600

    initial work on sd card

---
+++ Inc/sd.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "stm32f0xx.h"
+#include <stm32f072xb.h>
+#include "stm32f0xx_hal.h"
+#include "stm32f0xx_hal_spi.h"
+#include "stm32f0xx_hal_gpio.h"
+#include "stm32f0xx_hal_gpio_ex.h"
+
+
+void init_spi_sd(void);
+
+void init_sd(void);
---
+++ Src/sd.c
@@ -0,0 +1,53 @@
+#include "sd.h"
+//use fatfs for filesystem, requires implementation of hardware accesses.
+
+// sd card connect to spi2
+void init_spi_sd()
+{
+  //enable clocks
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+	RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+  RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+
+  //setup gpio for spi2
+  //pb12 NSS
+  //pb13 SCK
+  //pb14 MISO
+  //pb15 MOSI
+  // all use AF0
+  GPIO_InitTypeDef initSPI2 = {
+    .Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15,
+    .Mode = GPIO_MODE_AF_PP,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_LOW,
+    .Alternate = GPIO_AF0_SPI2
+  };
+  HAL_GPIO_Init(GPIOC, &initSPI2);
+
+  //configure spi2
+	SPI2->CR1 &= ~SPI_CR1_SPE; //make sure disabled
+
+  //set baud rate to minimum
+	SPI2->CR1 &= ~SPI_CR1_BR; 
+	SPI2->CR1 |=  SPI_CR1_BR_0;
+	SPI2->CR1 |=  SPI_CR1_BR_1;
+	SPI2->CR1 |=  SPI_CR1_BR_2;
+
+  //master mode, force NSS to 1
+	SPI2->CR1 |=  SPI_CR1_MSTR;
+	SPI2->CR1 |=  SPI_CR1_SSM;
+	SPI2->CR1 |=  SPI_CR1_SSI;
+
+  //8 bit data size
+	SPI2->CR2 &= ~SPI_CR2_DS;
+	SPI2->CR2 |=  (SPI_CR2_DS_0 | SPI_CR2_DS_1 | SPI_CR2_DS_2 | SPI_CR2_FRXTH);
+
+  //enable spi
+	SPI2->CR1 |=  SPI_CR1_SPE;
+}
+
+void init_sd()
+{
+
+}
